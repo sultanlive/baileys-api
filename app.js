3244d38ce47d5b137f23e931721672da6e2a9967
Anuar Buranbayev <anuar.buranbaev@gmail.com>
2022-06-16 22:28:54 +0600

    feat: logs at start/error and resources interval

--- app.js
+++ app.js
@@ -7,8 +7,32 @@ import seqLogger from './utils/seqLogger.js'
 
 process.on('uncaughtException', (error) => {
     const { message, stack } = error
-    seqLogger.fatal({ message, stack }, `API. Uncaught Error: ${message}`)
+    seqLogger.fatal({ message, stack }, `API. Uncaught Error: ${message}. PID: ${process.pid}`)
 })
+
+process.on('uncaughtExceptionMonitor', (error) => {
+    const { message, stack } = error
+    seqLogger.fatal({ message, stack }, `API. Uncaught Error Monitor: ${message}. PID: ${process.pid}`)
+})
+
+process.on('beforeExit', (code) => {
+    seqLogger.warn({ code }, `API. Server exit with code ${code}. PID: ${process.pid}`)
+})
+
+const ONE_MINUTE = 1000 * 60
+
+setInterval(() => {
+    const memory = process.memoryUsage()
+    const cpu = process.cpuUsage()
+    const appUpTime = Math.floor(process.uptime())
+    seqLogger.info(
+        {
+            memory,
+            cpu,
+        },
+        `API. Server log resources, uptime ${appUpTime / 60} min. PID: ${process.pid}`
+    )
+}, ONE_MINUTE)
 
 const app = express()
 const host = process.env.HOST ?? '127.0.0.1'
@@ -20,6 +44,10 @@ app.use('/', routes)
 
 app.listen(port, () => {
     init()
+    seqLogger.info(
+        { port, version: process.version, v8: process.versions.v8, platform: process.platform },
+        `API. Server started. PID: ${process.pid}`
+    )
     console.log(`Server is listening on http://${host}:${port}`)
 })
 
